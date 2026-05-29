7fbcce3bf9a654a1568e988d668b7bcfc4791124
Aqu1bK <aquibkhan7221@gmail.com>
2026-05-29 23:10:15 +0530

    fixed dev issue inngest

--- inngest/client.ts
+++ inngest/client.ts
@@ -6,6 +6,6 @@ export const inngest = new Inngest({
   name: "Personalized Newsletter Generator",
   signingKey: process.env.INNGEST_SIGNING_KEY,
   eventKey: process.env.INNGEST_EVENT_KEY,
-  mode: "cloud",
+  // mode: "cloud",
 });
 
