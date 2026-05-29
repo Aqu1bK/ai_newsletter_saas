b20bce5f5089f6f2666edb563c48b7dbb8505723
Aqu1bK <aquibkhan7221@gmail.com>
2026-05-29 22:00:52 +0530

    remove base url

--- inngest/client.ts
+++ inngest/client.ts
@@ -6,9 +6,5 @@ export const inngest = new Inngest({
   name: "Personalized Newsletter Generator",
   signingKey: process.env.INNGEST_SIGNING_KEY,
   eventKey: process.env.INNGEST_EVENT_KEY,
-
-  //For production
-  baseUrl: process.env.VERCEL_URL 
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
 });
+
